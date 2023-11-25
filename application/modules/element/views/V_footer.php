5f64a3ac9a7803bb3fe90e1f5a36ef63e43349d2
nrif_ <naftaryandikaisyacafahmi@gmail.com>
2023-11-25 14:12:09 +0700

    25112023 -  module wrok card

--- application/modules/element/views/V_footer.php
+++ application/modules/element/views/V_footer.php
@@ -9,10 +9,10 @@
   <script src="<?php echo base_url();?>assets/AdminLTE/plugins/jquery/jquery.min.js"></script>
   <!-- Bootstrap 4 -->
   <script src="<?php echo base_url();?>assets/AdminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-   <!-- Select2 -->
-   <script src="<?php echo base_url();?>assets/AdminLTE/plugins/select2/js/select2.full.min.js"></script>
-   <!-- date-range-picker -->
-<script src="../../plugins/daterangepicker/daterangepicker.js"></script>
+  <!-- Select2 -->
+  <script src="<?php echo base_url();?>assets/AdminLTE/plugins/select2/js/select2.full.min.js"></script>
+  <!-- date-range-picker -->
+  <script src="../../plugins/daterangepicker/daterangepicker.js"></script>
   <!-- DataTables  & Plugins -->
   <script src="<?php echo base_url();?>assets/AdminLTE/plugins/datatables/jquery.dataTables.min.js"></script>
   <script src="<?php echo base_url();?>assets/AdminLTE/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
